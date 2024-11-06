780ad43d8836aa8702437e954aa37df32bcafca4
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-11-06 17:42:54 +0000

    Update wordpress_automate.sh
    Uncommitted the commands for creating the database

--- wordpress_automate.sh
+++ wordpress_automate.sh
@@ -9,17 +9,17 @@ sudo apt -y install unzip
 # Install/Unzip/Remove WordPress 
 sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
-# sudo rm latest.zip
+sudo rm latest.zip
 
 # Create a MariaDB Database and a User for the WordPress Site  
-# sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
-# sudo mysql -e "CREATE USER wpuser@localhost identified by 'my_football'"
-# sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
-# sudo mysql -e "FLUSH PRIVILEGES" # Applies everything you've done 
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE USER wpuser@localhost identified by 'my_football'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "FLUSH PRIVILEGES" # Applies everything you've done 
 
 # Configure WordPress
-# cd wordpress/
-###  cp wp-config-sample.php wp-config.php 
+cd wordpress/
+cp wp-config-sample.php wp-config.php 
 
 # wget s3://iqrawordpressbucket/wp-config.php
 
