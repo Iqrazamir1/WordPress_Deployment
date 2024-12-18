5aeed46086e2801e072c93898d75a4ef6fb3eb32
Iqra Zamir <iqrazamir@Iqras-MacBook-Air.local>
2024-12-18 10:11:50 +0000

    Updated the git clone line to specify the development branch

--- git_automate.sh
+++ git_automate.sh
@@ -5,6 +5,6 @@ sudo apt -y update
 sudo apt -y upgrade
  
 cd /root/
-sudo git clone https://github.com/Iqrazamir1/EPA_WordPress_Website.git
+sudo git clone -b develop https://github.com/Iqrazamir1/EPA_WordPress_Website.git
 sudo chmod -R 755 EPA_WordPress_Website
 sudo bash EPA_WordPress_Website/lemp_stack_automate.sh
