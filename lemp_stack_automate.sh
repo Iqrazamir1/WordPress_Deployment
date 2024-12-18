d963e266a9c78ffabccf194185c2a11806a22957
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-12-18 13:54:30 +0000

    Update lemp_stack_automate.sh
    Splet SERVERNAME as SEVENNAME

--- lemp_stack_automate.sh
+++ lemp_stack_automate.sh
@@ -27,7 +27,7 @@ sudo mv /root/EPA_WordPress_Website/nginx.conf /etc/nginx/conf.d/nginx.conf
 dns_record=$(curl -s icanhazip.com | sed 's/^/ec2-/; s/\./-/g; s/$/.compute-1.amazonaws.com/')
 
 # Updates the Nginx config file with the server name of the EC2 Instance 
-sed -i "s/SEVENNAME/$dns_recored/g" /etc/nginx/conf.d/nginx.conf
+sed -i "s/SERVERNAME/$dns_recored/g" /etc/nginx/conf.d/nginx.conf
 
 # This will only reload nginx if the test is successful 
 nginx -t && systemctl reload nginx
