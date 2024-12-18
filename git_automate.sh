2b5052dab8b59c6f756e70592b33c6cf3e3d6afe
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-12-18 16:32:32 +0000

    Update git_automate.sh
    Updated path to lemp_stack_automate.sh

--- git_automate.sh
+++ git_automate.sh
@@ -7,4 +7,4 @@ sudo apt -y upgrade
 cd /root/
 sudo git clone -b develop https://github.com/Iqrazamir1/EPA_WordPress_Website.git
 sudo chmod -R 755 EPA_WordPress_Website
-sudo bash EPA_WordPress_Website/lemp_stack_automate.sh
+sudo bash /root/EPA_WordPress_Website/lemp_stack_automate.sh
