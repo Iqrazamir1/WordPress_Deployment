f8c2720ec8725eee7ddbf42c82bd7e45dfc9da12
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2025-01-25 19:45:05 +0000

    Update wordpress_automate.sh

--- wordpress_automate.sh
+++ wordpress_automate.sh
@@ -3,6 +3,9 @@
 # Entering the html directory 
 cd /var/www/html
 
+# Install the AWS CLI tool using Snap for managing AWS resources
+snap install aws-cli --classic
+
 # Install the unzip package 
 sudo apt -y install unzip
 
@@ -25,6 +28,10 @@ echo $password > creds.txt
 # sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
 # sudo mysql -e "FLUSH PRIVILEGES" # Applies everything you've done 
 
+sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wp-config.php
+sudo chmod 640 /var/www/html/wp-config.php 
+sudo chown -R www-data:www-data /var/www/html/wp-config.php
+
 # Connect to S3 Bucket
 aws s3 cp s3://mariadbdatabase/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
 sudo gunzip /tmp/wordpress_dump.sql.gz
@@ -32,9 +39,9 @@ sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
 sudo mysql $username < /tmp/wordpress_dump.sql
 sudo rm /tmp/wordpress_dump.sql
 
-sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wp-config.php
-sudo chmod 640 /var/www/html/wp-config.php 
-sudo chown -R www-data:www-data /var/www/html/wp-config.php
+# sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wp-config.php
+# sudo chmod 640 /var/www/html/wp-config.php 
+# sudo chown -R www-data:www-data /var/www/html/wp-config.php
 
 sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
 sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
