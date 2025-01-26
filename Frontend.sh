592ae02507c4cf467f93030c331182cbd7050eae
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2025-01-26 23:27:03 +0000

    Create Frontend.sh

---
+++ Frontend.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Log file path
+LOG_FILE="/var/log/script_execution.log"
+
+# Function to check the exit status of the last executed command
+check_exit_status() {
+    if [ $? -ne 0 ]; then
+        echo "Error: $1 failed." | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "$1 succeeded." | tee -a $LOG_FILE
+    fi
+}
+
+# Clear the log file at the beginning of the script
+> $LOG_FILE
+
+# Update and Upgrade package lists
+echo "Running apt update..." | tee -a $LOG_FILE
+sudo apt -y update && sudo apt -y upgrade
+check_exit_status "apt update and upgrade"
+
+# Clone the GitHub repository
+echo "Cloning GitHub repository..." | tee -a $LOG_FILE
+sudo git clone -b develop https://github.com/Iqrazamir1/WordPress_Deployment.git /root/WordPress_Deployment
+check_exit_status "git clone"
+
+# Change permissions of the cloned repository
+echo "Changing permissions of the cloned repository..." | tee -a $LOG_FILE
+sudo chmod -R 755 /root/WordPress_Deployment
+check_exit_status "chmod"
+
+# Run the setup script
+log "Running lemp-setup.sh script..."
+
+sudo apt -y update && sudo apt -y upgrade
+sudo touch /root/testing.txt
+sudo apt -y install nginx
+sudo systemctl start nginx && sudo systemctl enable nginx 
+sudo systemctl status nginx > /root/testing.txt
+sudo apt -y install php-fpm php php-cli php-common php-imap php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
+sudo php -v >> /root/testing.txt
+
+sudo mv /root/WordPress_Deployment/configs/nginx.conf /etc/nginx/conf.d/nginx.conf
+
+# Update nginx configuration file
+sed -i "s/SERVERNAME/$dns_record/g" /etc/nginx/conf.d/nginx.conf
+nginx -t && systemctl reload nginx 
+
+# Update package list and install Certbot and Certbot Nginx plugin
+sudo apt -y update && sudo apt -y upgrade
+sudo apt -y install certbot
+sudo apt -y install python3-certbot-nginx
+
+# Define your email
+EMAIL=EMAIL
+DOMAIN=DOMAIN
+
+sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
+
+# Nginx unit test that will reload Nginx to apply changes ONLY if the test is successful
+sudo nginx -t && systemctl reload nginx
+
+# Install WordPress
+cd /var/www/html
+sudo apt -y install unzip 
+sudo wget https://wordpress.org/latest.zip 
+sudo unzip latest.zip  
+sudo rm latest.zip 
+
+sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sudo chmod 640 /var/www/html/wp-config.php 
+sudo chown -R www-data:www-data /var/www/html/
+
+SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
+STRING='put your unique phrase here'
+printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/html/wp-config.php
