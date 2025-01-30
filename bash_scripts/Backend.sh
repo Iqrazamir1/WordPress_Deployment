aade16ce2de3fcb18c85b0581dd0c814fb541f2e
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2025-01-30 15:59:33 +0000

    commented my code

--- bash_scripts/Backend.sh
+++ bash_scripts/Backend.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
+# Update and upgrade system packages
 sudo apt -y update && sudo apt -y upgrade
 
 # Install the AWS CLI tool using Snap for managing AWS resources
 snap install aws-cli --classic
 
+# Install MariaDB server and client
 apt -y install mariadb-server mariadb-client
 
+# Modify MariaDB configuration to allow external connections
 sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# Restart MariaDB service and check if it is running
 mysqladmin ping && systemctl restart mariadb
 
 # Database username and password variables
 username=DB_USERNAME
 password=DB_PASSWORD
 
+# Store credentials in a temporary file for backup
 echo $username > creds.txt
 echo $password >> creds.txt
 
-# Connect to S3 Bucket
+# Download and restore the WordPress database backup from S3
 aws s3 cp s3://mariadbdatabase/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
 sudo gunzip /tmp/wordpress_dump.sql.gz
+
+# Create the database and user if they do not exist
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
 sudo mysql -e "CREATE USER IF NOT EXISTS '$username'@'FRONTEND_IP' IDENTIFIED BY '$password'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO '$username'@'FRONTEND_IP'"
 sudo mysql -e "FLUSH PRIVILEGES"
+
+# Restore the database backup
 sudo mysql $username < /tmp/wordpress_dump.sql
 sudo rm /tmp/wordpress_dump.sql
 
-# This securely stores the credentials file in AWS S3 for later use or backup
+# Securely store the credentials file in AWS S3 for later use or backup
 aws s3 cp creds.txt s3://mariadbdatabase
